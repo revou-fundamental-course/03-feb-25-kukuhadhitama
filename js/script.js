27f5dd476057de4678f61c45276a9a04ed6ec533
kukuhadhitama <kukuhadhitama.business@gmail.com>
2025-02-13 13:39:07 +0700

    commit validasi js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@
 console.log('Javascript is working');
 
 function validateForm() {
+    let input = document.getElementById('main-input')
+    console.log(input.value);
+
+    if(input.value == '') {
+        alert('silahkan isi data pada kolom ini');
+    } else {
+        console.log('berhasil di eksekusi'); 
+        alert('selamat anda berhasil, semoga terbantu')
+    }
     ///Algoritma flow
     console.log('berhasil di eksekusi');
+
+    console.log(document.getElementById('main-input'));
 }
 
